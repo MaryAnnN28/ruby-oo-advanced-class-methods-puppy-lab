8bef7b5def5db64d67616434fbc5daa8b13495a1
MaryAnnN28 <maryann.navarrete@gmail.com>
2020-11-20 23:51:10 -0500

    done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,36 @@
 # Add your code here
+class Dog
+
+     attr_reader :name
+
+     @@all = []
+
+     def initialize(name)
+          @name = name
+          save  #this method (save from below) gets called at initialization when new dog is created
+     end
+
+     def self.all
+          @@all
+     end
+
+     def self.clear_all
+          @@all.clear
+     end
+     
+     # puts out the names of each dog (via class method)
+     # hint - initially :name was not in reader, writer or accessor.  
+     # needed an attr_reader to be able to read at initialization for this method
+     def self.print_all              
+          @@all.each do | dog |
+               puts dog.name
+          end
+     end
+
+     def save 
+          @@all << self
+          @@all
+     end
+
+       
+end
